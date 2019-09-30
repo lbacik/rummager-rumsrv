4fe4370b299be20583915a3e9a34dba3d2637199
Łukasz Bacik <mail@luka.sh>
2019-09-30 20:36:05 +0200

    Api - DI container - objects access method refactor

--- src/Rummager/Service/Api.php
+++ src/Rummager/Service/Api.php
@@ -4,108 +4,124 @@ declare(strict_types=1);
 
 namespace Rummager\Service;
 
+use Psr\Container\ContainerInterface;
+
 class Api
 {
-    /** @var ServiceProviderInterface */
+    /** @var ContainerInterface */
     private $serviceProvider;
 
-    public function __construct(ServiceProviderInterface $serviceProvider)
+    public function __construct(ContainerInterface $serviceProvider)
     {
         $this->serviceProvider = $serviceProvider;
     }
 
-    public function getHostID(string $name): int
+    public function getHostId(string $name): int
     {
         return $this
-            ->serviceProvider['host']
+            ->serviceProvider
+            ->get('host')
             ->hostIdByName($name);
     }
 
     public function getNodesRunning(int $hostId): int
     {
         return $this
-            ->serviceProvider['host']
+            ->serviceProvider
+            ->get('host')
             ->nodesRunning($hostId);
     }
 
     public function getHostMaxNodes(int $hostId): int
     {
         return $this
-            ->serviceProvider['host']
+            ->serviceProvider
+            ->get('host')
             ->getMaxNodes($hostId);
     }
 
     public function getHostMaxThreads(int $hostId): int
     {
         return $this
-            ->serviceProvider['host']
+            ->serviceProvider
+            ->get('host')
             ->getMaxThreads($hostId);
     }
 
     public function checkNodeIsRunning(int $nodeId): bool
     {
         return $this
-            ->serviceProvider['host']
+            ->serviceProvider
+            ->get('host')
             ->checkNodeIsRunning($nodeId);
     }
 
     public function getNewNodeId(int $hostId): int
     {
         return $this
-            ->serviceProvider['host']
+            ->serviceProvider
+            ->get('host')
             ->getNewNodeId($hostId);
     }
 
     public function getNetwork(int $networkId): string
     {
         return $this
-            ->serviceProvider['network']
+            ->serviceProvider
+            ->get('network')
             ->getNetworkAsCidr($networkId);
     }
 
     public function getLastIp(string $ip, string $brd): string
     {
         return $this
-            ->serviceProvider['smtp']
+            ->serviceProvider
+            ->get('smtp')
             ->lastIp($ip, $brd);
     }
 
     public function updateNetworkStatus(string $netAddr, string $mask, int $newStatus): void
     {
         $this
-            ->serviceProvider['network']
+            ->serviceProvider
+            ->get('network')
             ->updateStatus($netAddr, $mask, $newStatus);
     }
 
     public function startNewCheck(int $nodeId, int $moduleId): int
     {
         return $this
-            ->serviceProvider['checkProcess']
+            ->serviceProvider
+            ->get('checkProcess')
             ->startNew($nodeId, $moduleId);
     }
 
     public function getNetworkId(int $checkId)
     {
         return $this
-            ->serviceProvider['checkProcess']
+            ->serviceProvider
+            ->get('checkProcess')
             ->getNetworkId($checkId);
     }
 
     public function getModuleId(string $name): int
     {
         return $this
-            ->serviceProvider['module']
+            ->serviceProvider
+            ->get('module')
             ->moduleIdByName($name);
     }
 
-    public function addHostInfo(array $data)
+    public function addHostInfo(array $data): void
     {
         $moduleId = $this
-            ->serviceProvider['checkProcess']
+            ->serviceProvider
+            ->get('checkProcess')
             ->getModuleId($data['checkId']);
 
         $this
-            ->serviceProvider['module']
+            ->serviceProvider
+            ->get('module')
             ->addData($moduleId, $data);
     }
 }
